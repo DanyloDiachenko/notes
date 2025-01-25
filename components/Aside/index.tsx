99d16b7c70e19c30f149940aa0f113ec097c151a
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-25 22:23:04 +0200

    _

--- components/Aside/index.tsx
+++ components/Aside/index.tsx
@@ -24,12 +24,14 @@ export const Aside = ({ pathname }: AsideProps) => {
     return (
         <aside className="w-full px-4 pt-4 text-gray-600 border-r-2 border-gray-200">
             <div className="border-b-2 border-gray-200 pb-4">
-                <Image
-                    src="/icons/logo.png"
-                    width={192}
-                    height={70}
-                    alt="Logo"
-                />
+                <Link href="/">
+                    <Image
+                        src="/icons/logo.png"
+                        width={192}
+                        height={70}
+                        alt="Logo"
+                    />
+                </Link>
                 <Link
                     className={`mt-6 p-2 px-4 flex justify-between items-center rounded-md
                     text-gray-800 hover:bg-blue-50 hover:text-gray-800 ${
