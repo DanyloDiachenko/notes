585837eca2b18d6c2b7f71486258eed258ab4952
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-25 21:18:05 +0200

    _

--- app/[type]/layout.tsx
+++ app/[type]/layout.tsx
@@ -1,10 +1,28 @@
 import { Notes } from "@/components/Notes";
+import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 
-const NoteTypeLayout = ({
+const NoteTypeLayout = async ({
     children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) => {
+    params,
+}: {
+    children: ReactNode;
+    params: Promise<{ type: string }>;
+}) => {
+    const paramsData = await params;
+
+    switch (paramsData.type) {
+        case "all": {
+            break;
+        }
+        case "archived": {
+            break;
+        }
+        default: {
+            return notFound();
+        }
+    }
+
     return (
         <>
             <div className="grid grid-cols-[0.25fr_0.5fr_0.25fr]">
