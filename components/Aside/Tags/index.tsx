67445cf077c30e4059a58fdce4a682fc91300de4
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-25 23:34:42 +0200

    _

---
+++ components/Aside/Tags/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { TagsProps } from "./tags.props";
+import { usePathname, useSearchParams } from "next/navigation";
+import { HiOutlineTag } from "react-icons/hi2";
+
+export const Tags = ({ tags }: TagsProps) => {
+    const clientPathname = usePathname();
+    const tagSearchParam = useSearchParams().get("tag");
+
+    return (
+        <div className="px-4 mt-2">
+            <div className="text-gray-500 font-semibold">Tags</div>
+            {tags.map((tag, index) => (
+                <Link
+                    key={index}
+                    className={`mt-4 flex items-center gap-2 text-gray-500 duration-0`}
+                    href={`${
+                        clientPathname.includes("all") ? "/all" : "/archived"
+                    }?tag=${tag.slug}`}
+                >
+                    <HiOutlineTag
+                        className={`size-5 duration-300 ${
+                            tagSearchParam === tag.slug ? "text-[#7351f5]" : ""
+                        }`}
+                    />
+                    <span
+                        className={`font-medium duration-300 ${
+                            tagSearchParam === tag.slug ? "text-[#7351f5]" : ""
+                        }`}
+                    >
+                        {tag.title}
+                    </span>
+                </Link>
+            ))}
+        </div>
+    );
+};
--- components/Aside/index.tsx
+++ components/Aside/index.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import Image from "next/image";
-import { HiOutlineTag } from "react-icons/hi2";
 import Link from "next/link";
 import { AsideProps } from "./aside.props";
-import { usePathname, useSearchParams } from "next/navigation";
 import { Categories } from "./Categories";
+import { Tags } from "./Tags";
 
 const tags = [
     { title: "Cooking", slug: "cooking" },
@@ -16,9 +13,6 @@ const tags = [
 ];
 
 export const Aside = ({ pathname }: AsideProps) => {
-    const clientPathname = usePathname();
-    const tagSearchParam = useSearchParams().get("tag");
-
     return (
         <aside className="w-full px-4 pt-4 text-gray-600 border-r-2 border-gray-200">
             <div className="border-b-2 border-gray-200 pb-4">
@@ -32,37 +26,7 @@ export const Aside = ({ pathname }: AsideProps) => {
                 </Link>
                 <Categories pathname={pathname} />
             </div>
-            <div className="px-4 mt-2">
-                <div className="text-gray-500 font-semibold">Tags</div>
-                {tags.map((tag, index) => (
-                    <Link
-                        key={index}
-                        className={`mt-4 flex items-center gap-2 text-gray-500 duration-0`}
-                        href={`${
-                            clientPathname.includes("all")
-                                ? "/all"
-                                : "/archived"
-                        }?tag=${tag.slug}`}
-                    >
-                        <HiOutlineTag
-                            className={`size-5 duration-300 ${
-                                tagSearchParam === tag.slug
-                                    ? "text-[#7351f5]"
-                                    : ""
-                            }`}
-                        />
-                        <span
-                            className={`font-medium duration-300 ${
-                                tagSearchParam === tag.slug
-                                    ? "text-[#7351f5]"
-                                    : ""
-                            }`}
-                        >
-                            {tag.title}
-                        </span>
-                    </Link>
-                ))}
-            </div>
+            <Tags tags={tags} />
         </aside>
     );
 };
