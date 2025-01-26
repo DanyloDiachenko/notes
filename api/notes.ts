021078b81afb92da28fbe7ded60867cf8a8e4d90
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-26 19:44:57 +0200

    apiS

---
+++ api/notes.ts
@@ -0,0 +1,9 @@
+export const getNotes = () => {};
+
+export const getNote = () => {};
+
+export const updateNote = () => { };
+
+export const deleteNote = () => { };
+
+export const createNote = () => {};
---
+++ api/tags.ts
@@ -0,0 +1,7 @@
+export const getTags = () => {};
+
+export const editTag = () => {};
+
+export const deleteTag = () => {};
+
+export const createTag = () => {};
