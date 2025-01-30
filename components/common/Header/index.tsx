f92172b2a55f259a4aabb8779f5369c48c5c6294
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-30 14:36:14 +0200

    auth styles

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -9,6 +9,7 @@ import { getHeadingText } from "./getHeadingText";
 import { IoIosLogIn } from "react-icons/io";
 import { useDispatch } from "react-redux";
 import { setOpenedModal } from "@/store/slices/openedModal";
+import { Button } from "@/components/ui/Button";
 
 export const Header = ({ pathname, isAuthorized }: HeaderProps) => {
     const dispatch = useDispatch();
@@ -28,18 +29,25 @@ export const Header = ({ pathname, isAuthorized }: HeaderProps) => {
         <header className="p-8 flex justify-between items-center border-b-2 border-gray-200">
             <h1 className="font-bold text-4xl">{headingText}</h1>
             <div className="flex items-center gap-4">
-                <div className="relative w-[300px]">
-                    <input
-                        type="text"
-                        className="w-full border-2 border-gray-200 rounded-md p-2 pl-8"
-                        placeholder="Search in..."
-                    />
-                    <FaSearch className="absolute left-2 top-3 size-5 opacity-20" />
-                </div>
+                {isAuthorized && (
+                    <div className="relative w-[300px]">
+                        <input
+                            type="text"
+                            className="w-full border-2 border-gray-200 rounded-md p-2 pl-8"
+                            placeholder="Search in..."
+                        />
+                        <FaSearch className="absolute left-2 top-3 size-5 opacity-20" />
+                    </div>
+                )}
                 {!isAuthorized && (
-                    <button onClick={onLoginClick}>
-                        <IoIosLogIn className="size-8 text-gray-400" />
-                    </button>
+                    <Button
+                        color="gray"
+                        onClick={onLoginClick}
+                        className="p-2 w-auto px-5 gap-2"
+                    >
+                        <IoIosLogIn className="size-6 text-gray-400" />
+                        Authorization
+                    </Button>
                 )}
             </div>
         </header>
