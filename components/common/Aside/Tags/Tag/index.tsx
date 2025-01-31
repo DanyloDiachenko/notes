37de517f831bc38d659b0377f3893f26b133d5ad
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-31 13:49:52 +0200

    fix

--- components/common/Aside/Tags/Tag/index.tsx
+++ components/common/Aside/Tags/Tag/index.tsx
@@ -2,27 +2,23 @@
 
 import Link from "next/link";
 import { Actions } from "./Actions";
-import { useDispatch } from "react-redux";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Modal } from "@/interfaces/common/modal.type";
-import { setOpenedModal } from "@/store/slices/openedModal";
-import { setTagToEdit } from "@/store/slices/tagToEdit";
-import { Tag as ITag } from "@/interfaces/common/tag.interface";
 import { HiOutlineTag } from "react-icons/hi2";
 import { TagProps } from "./tag.props";
 
 export const Tag = ({ tag }: TagProps) => {
-    const dispatch = useDispatch();
     const clientPathname = usePathname();
     const tagSearchParam = useSearchParams().get("tag");
+
+    const tagHref = `${
+        clientPathname.includes("archived") ? "/archived" : "/all"
+    }?tag=${tag.slug}`;
 
     return (
         <div className={`mt-4 flex items-center justify-between`}>
             <Link
                 className="flex items-center gap-2 duration-0 text-gray-500"
-                href={`${
-                    clientPathname.includes("all") ? "/all" : "/archived"
-                }?tag=${tag.slug}`}
+                href={tagHref}
             >
                 <HiOutlineTag
                     className={`size-5 duration-300 ${
