3fb105a89f0d5076033228eacb00148ad8c5e1b6
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-04 15:38:46 +0200

    fixes

--- components/modals/index.tsx
+++ components/modals/index.tsx
@@ -4,6 +4,7 @@ import { closeModal, selectOpenedModal } from "@/store/slices/openedModal";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { IoMdClose } from "react-icons/io";
 import { modals } from "./modals";
+import { cn } from "@/helpers/cn";
 
 export const Modals = () => {
     const dispatch = useAppDispatch();
@@ -15,27 +16,22 @@ export const Modals = () => {
 
     const currentModal = modals.find((modal) => modal.key === openedModal);
 
-    if (!currentModal) {
-        return <></>;
-    }
-
     return (
         <>
             <div
-                className={`fixed top-0 right-0 left-0 bottom-0 bg-slate-900 w-full h-full transition-[opacity]  ${
+                className={cn(
+                    "fixed top-0 right-0 left-0 bottom-0 bg-slate-900 w-full h-full transition-[opacity]",
                     openedModal
                         ? "opacity-30 z-10 duration-500 "
-                        : "opacity-0 -z-10 duration-0"
-                }`}
+                        : "opacity-0 -z-10 duration-0",
+                )}
                 onClick={closeModalHandler}
             ></div>
             <div
-                className={`max-w-[700px] w-full absolute bg-white right-[50%] left-[50%] translate-x-[-50%]
-                    p-6 transition-[opacity] ${
-                        openedModal
-                            ? "top-20 z-20 opacity-100 duration-500"
-                            : "-z-10 opacity-0 duration-0"
-                    }`}
+                className={cn(
+                    "max-w-[700px] w-full absolute bg-white right-[50%] left-[50%] translate-x-[-50%] p-6 transition-[opacity] opacity-0 top-20",
+                    openedModal ? "z-20 opacity-100 duration-500" : "-z-10",
+                )}
             >
                 <button
                     className="absolute top-2 right-3"
@@ -43,7 +39,7 @@ export const Modals = () => {
                 >
                     <IoMdClose />
                 </button>
-                {currentModal.component}
+                {currentModal?.component}
             </div>
         </>
     );
